7e579a01ba4503764ee670350c87c95b2529ed10
heathshults <heatheshults@gmail.com>
2020-07-16 23:49:26 -0500

    render assets

--- scripts/render-scripts.js
+++ scripts/render-scripts.js
@@ -1,37 +1,110 @@
 // 'use strict';
-var path = require('path');
-var md = require('./mkdir-structure');
-var browserfry = require('./render-scripts-browserify')
-var whoDidIt = require("./goCreds");
+/* eslint-disable no-octal */
+var nfs = require('node-fs');
+var fs = require('fs')
+var path = require('path')
+var browserify = require('browserify')
+var babelify = require('babelify')
 var defaultInFile = path.resolve(__dirname, '../src/index.js')
-var destPath = path.resolve(__dirname, '../www/assets/js/')
 var outFile = path.resolve(__dirname, '../www/assets/js/HeathScript.built.js')
+var destPath = path.resolve(__dirname, '../www/assets/js/')
+// var whoDidIt = require("./goCreds.js");
+
 console.log('starting up heathenScriptJS')
 
-  // if (typeof callback == "function") {
-  //   console.log('Created HeathenScript.built.js')
-  //   callback('Success');
+function HeathenScriptJS() {
+
+  /**
+   * create js directory if not exist
+   *
+   * @param {*} path  string
+   */
+  function makeDirectory(path) {
+    console.log(path)
+
+    nfs.mkdir(path, 0777, true, function (err) {
+      if (err) {
+        console.log(err);
+        return `Error making path: ${err}`
+      } else {
+        console.log(`Created: ${path}`);
+        return 'Success'
+      }
+    });
+  }
+  exports.makeDirectory = makeDirectory
+  makeDirectory(destPath)
+  // var mkdr = makeDirectory(destPath)
+  // if (mkdr === 'Success') {
+  //   console.log('MakeDirectory Succeeded.')
+  // } else {
+  //   return 'JS Fail'
   // }
 
 
-var heathenScriptJS = new Promise(function(resolve, reject) {
-  console.log('Inside heathenScriptJS')
 
-  resolve = md.makeDirectory(destPath);
-  if (resolve === 'Success') return resolve
-    else return reject
+  /**
+   * @name processJS
+   * babelifies, browserifies and
+   * concatenates the main js file
+   *
+   * @param {*} jsFile string
+   * @param {*} callback function
+   */
+  function processJS(jsFile, callback) {
+    console.log('inside procesJS')
+    if (!jsFile) jsFile = defaultInFile
 
-}).then(function(result) { // (**)
+    // make the js file and save it to the makeDirectory path
+    browserify({
+        debug: true
+      })
+      .transform(babelify)
+      .require(jsFile, {
+        entry: true
+      })
+      .bundle()
+      .on("error", function (err) {
+        console.log("Error: " + err.message)
+        console.log("Using default JS file...")
+        processJS(defaultInFile)
 
-  if (result === 'Success') browserfry.processJS(defaultInFile)
-  return result
+        return 'damn it! ', err.message
+      })
+      .pipe(fs.createWriteStream(outFile));
 
-}).then(function(result) { // (***)
+     fs.access(outFile, (err) => {
+      if (err) {
+        console.log("File not created. Check file passed. Check this script for further debuggin.");
+        return err
+      } else {
+        console.log("File created");
+        return 'Success'
+      }
+    })
 
- if (result === 'Success') whoDidIt.goCreds(outFile)
-  return result
+    if (typeof callback === 'function'){callback()}
+  }
+  exports.processJS = processJS
 
-})
 
-exports.heathenScriptJS = heathenScriptJS
+  var projs = processJS(defaultInFile)
+  if (projs === 'Success') {
+    console.log('JS Compiled Successfully')
+  } else {
+    return 'JS Fail'
+  }
 
+  // create the banner
+  // var whoMadeThis = (cb) => {
+  //   console.log('starting up whoMadeThis()')
+  //   var who = whoDidIt.goCreds(outFile)
+  //   if (who === 'Success') return cb('Success')
+  //     else return 'FAIL BANNER'
+  // }
+  // exports.whoMadeThis = whoMadeThis
+
+
+}
+exports.HeathenScriptJS = HeathenScriptJS
+HeathenScriptJS()
