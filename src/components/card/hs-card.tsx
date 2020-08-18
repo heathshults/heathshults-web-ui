e534f1637f2edefc64b3bb70370c221704410782
heathshults <heatheshults@gmail.com>
2020-08-17 22:05:02 -0500

    cards

--- src/components/card/hs-card.tsx
+++ src/components/card/hs-card.tsx
@@ -13,11 +13,18 @@ export class HSCard {
   @Prop() colorTone: string;
   @Prop() cardId: string;
   @Prop() cardSize: string;
+  @Prop() imgHeaderImg: HTMLElement;
 
   componentWillLoad() {
+    this.imgHeaderImg = this.el.shadowRoot.querySelector('#hsHeaderImg');
+    if (this.imgHeaderImg) {
+      this.imgHeaderImg.style.width = '265px'
+      this.imgHeaderImg.style.height = '177px'
+    }
   }
 
   render() {
+
     return (
       <div id={this.cardId} class={`hs-card hs-card-size${this.cardSize} ${this.colorTone}`}>
         <slot />
