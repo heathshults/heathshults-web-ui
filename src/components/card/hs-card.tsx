991cdf24585c58db29bebe59defa1f1923d565d7
heathshults <heatheshults@gmail.com>
2020-09-05 09:03:39 -0500

    cards

--- src/components/card/hs-card.tsx
+++ src/components/card/hs-card.tsx
@@ -74,7 +74,33 @@ export class HSCard {
         <slot name="card-body" />
       
       <div class={`hs-card_footer ${this.colorToneClass} p-0`}>
-        <slot name="card-footer" />
+      <div class="row m-0 d-flex w-100">
+      <div class="col-md-12 p-3 hs-logo-row--footer">
+        <ul class="flex-container space-between">
+          <li class="flex-item">
+            <img src="/assets/img/logos/dmHTML5_Logo_512.png" class="hs-dev-logo" width="24" alt="HTML5 logo" />
+          </li>
+          <li class="flex-item">
+            <img src="/assets/img/logos/Sass-150.png" class="hs-dev-logo" width="24" alt="sass logo" />
+          </li>
+          <li class="flex-item">
+            <img src="/assets/img/logos/dm-css3-150.png" class="hs-dev-logo" width="24" alt="css logo" />
+          </li>
+          <li class="flex-item">
+            <img src="/assets/img/logos/js-logo.jpg" class="hs-dev-logo" width="24" alt="javascript logo" />
+          </li>
+          <li class="flex-item">
+            <img src="/assets/img/logos/dm-Node.png" class="hs-dev-logo" width="24" alt="node js logo" />
+          </li>
+          <li class="flex-item">
+            <img src="/assets/img/logos/gulp-150.png" class="hs-dev-logo" width="24" alt="gulp logo" />
+          </li>
+          <li class="flex-item">
+            <img src="/assets/img/logos/dm-Git-logo.png" class="hs-dev-logo" width="24" alt="git logo" />
+          </li>
+        </ul>
+      </div>
+      </div>
       </div>
       </div>
     );
