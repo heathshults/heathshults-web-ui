671ada8036ec16ec4a6ce504b18d1396a992402c
heathshults <heatheshults@gmail.com>
2020-10-15 22:10:24 -0500

    cssvars

---
+++ src/components/hs-button/hs-button.tsx
@@ -0,0 +1,20 @@
+import { Component, h } from '@stencil/core';
+
+
+@Component({
+    tag: 'hs-button',
+    styleUrl: 'hs-button.css',
+    shadow: true
+})
+export class HsButton {
+
+    
+
+    render() {
+        return (
+            <div>
+                <p>Hello HsButton!</p>
+            </div>
+        );
+    }
+}
