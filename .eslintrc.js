3313314fdf741210ab6e375b9e02d083f6fc23fa
heathshults <heatheshults@gmail.com>
2020-11-27 07:27:40 -0600

    new timeline and scss and css vars

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,29 @@
 module.exports = {
-  root: true,
-  parserOptions: {
-    ecmaVersion: 2017,
-    sourceType: 'module',
-  },
-  "env": {
-    "browser": true,
-    "node": true
-  },
-  "extends": [
-    "eslint:recommended"
-  ],  
-  "rules": {
-    'no-console': 1,
-    'no-undef': 1,
-    'no-unused-vars': 1,
-    'no-var-requires': 0,
-    'semi': [2, 'always', {
-      'omitLastInOneLineBlock': true
-    }]
-  }
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": 12
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+        'no-console': 1,
+        'no-undef': 1,
+        'no-unused-vars': 1,
+        'no-var-requires': 0,
+        'semi': [2, 'always', {
+            'omitLastInOneLineBlock': true
+        }],
+        "@typescript-eslint/no-this-alias": 0
+    }
 };
---
+++ src/js/modules/hs-timeline/hs-timeline-ts.ts
@@ -0,0 +1,71 @@
+((): void=>{
+  // Vertical Timeline - by CodyHouse.co
+	function VerticalTimeline( element: HTMLElement ) {
+		this.element = element;
+		this.blocks = this.element.getElementsByClassName("cd-timeline__block");
+		this.images = this.element.getElementsByClassName("cd-timeline__img");
+		this.contents = this.element.getElementsByClassName("cd-timeline__content");
+		this.offset = 0.8;
+		this.hideBlocks();
+	}
+
+	VerticalTimeline.prototype.hideBlocks = function(): void {
+		if ( !"classList" in document.documentElement ) {
+			return; // no animation on older browsers
+		}
+		//hide timeline blocks which are outside the viewport
+		const self: any = this;
+		for( let i = 0; i < this.blocks.length; i++) {
+			(function(i){
+				if( self.blocks[i].getBoundingClientRect().top > window.innerHeight*self.offset ) {
+					self.images[i].classList.add("cd-timeline__img--hidden"); 
+					self.contents[i].classList.add("cd-timeline__content--hidden"); 
+				}
+			})(i);
+		}
+	};
+
+	VerticalTimeline.prototype.showBlocks = function(): void {
+		if ( ! "classList" in document.documentElement ) {
+			return;
+		}
+		const self: any = this;
+		for( let i = 0; i < this.blocks.length; i++) {
+			(function(i): void{
+				if( self.contents[i].classList.contains("cd-timeline__content--hidden") && self.blocks[i].getBoundingClientRect().top <= window.innerHeight*self.offset ) {
+					// add bounce-in animation
+					self.images[i].classList.add("cd-timeline__img--bounce-in");
+					self.contents[i].classList.add("cd-timeline__content--bounce-in");
+					self.images[i].classList.remove("cd-timeline__img--hidden");
+					self.contents[i].classList.remove("cd-timeline__content--hidden");
+				}
+			})(i);
+		}
+	};
+
+	const verticalTimelines: HTMLCollectionOf<Element> = document.getElementsByClassName("js-cd-timeline");
+	const	verticalTimelinesArray: Array = [];
+	const	scrolling = false;
+	if( verticalTimelines.length > 0 ) {
+		for( let i = 0; i < verticalTimelines.length; i++) {
+			(function(i): void {
+				verticalTimelinesArray.push(new VerticalTimeline(verticalTimelines[i]));
+			})(i);
+		}
+
+		//show timeline blocks on scrolling
+		window.addEventListener("scroll", function(event): void {
+			if( !scrolling ) {
+				scrolling = true;
+				(!window.requestAnimationFrame) ? setTimeout(checkTimelineScroll, 250) : window.requestAnimationFrame(checkTimelineScroll);
+			}
+		});
+	}
+
+	function checkTimelineScroll(): void {
+		verticalTimelinesArray.forEach(function(timeline){
+			timeline.showBlocks();
+		});
+		scrolling = false;
+	}
+})();
