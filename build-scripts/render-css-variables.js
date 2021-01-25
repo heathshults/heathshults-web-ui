7804f59d0f35948b2262960b77629005c2a62fff
heathshults <heatheshults@gmail.com>
2021-01-25 06:57:18 -0600

    css  var converter

--- build-scripts/render-css-variables.js
+++ build-scripts/render-css-variables.js
@@ -1,23 +1,22 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const path = require('path');
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-require('@babel/register')({
-  presets: ['@babel/preset-env', '@babel/preset-react'],
-  plugins: [
-    '@babel/plugin-syntax-dynamic-import',
-    '@babel/plugin-proposal-class-properties',
-    '@babel/plugin-transform-runtime',
-    ['import', {libraryName: '@material-ui/core'}],
-  ],
-});
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
+
 const sassToCssVars = require('../src/js/modules/sass-to-css-variables');
 
-const scssVarFile = path.resolve(__dirname, '../src/scss/variables/_colors.scss');
-const cssVarFile = path.resolve(__dirname, '../src/scss/variables/_css-vars2.css');
+const files = [
+  {
+    'scssVarFile': path.resolve(__dirname, '../src/scss/variables/_colors.scss'),
+    'cssVarFile': path.resolve(__dirname, '../src/scss/variables/_css-vars2.scss')
+  },
+  {
+    'scssVarFile': path.resolve(__dirname, 'src/scss/bootstrap/scss/_variables.scss'),
+    'cssVarFile': path.resolve(__dirname, '../src/scss/variables/_css-vars-bootstrap.scss')
+  }
+]
 
-sassToCssVars
-  .convert(scssVarFile, cssVarFile)
-  .then((result) => console.log(result))
-  .catch((error) => console.log(error));
+files.forEach(file => {
+  sassToCssVars.convert(file.scssVarFile, file.cssVarFile)
+    .then(result => console.log(result))
+    .catch(error => console.log(error));
+
+})
