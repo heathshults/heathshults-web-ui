4f4bc0882dfd78b83b7c1e438f8d8ba19c23a282
heathshults <heatheshults@gmail.com>
2021-02-10 22:00:38 -0600

    3d rotator

--- src/js/modules/hs-3d-rotate/hs-3d-rotate.js
+++ src/js/modules/hs-3d-rotate/hs-3d-rotate.js
@@ -1,64 +1,87 @@
-var HS3DRotator = document.querySelector('.hs-3d-rotator');
-var cells = HS3DRotator.querySelectorAll('.hs-3d-rotator__cell');
-var cellCount; // cellCount set from cells-range input value
-var selectedIndex = 0;
-var cellWidth = HS3DRotator.offsetWidth;
-var cellHeight = HS3DRotator.offsetHeight;
-var isHorizontal = true;
-var rotateFn = isHorizontal ? 'rotateY' : 'rotateX';
-var radius;
-var theta;
-// console.log( cellWidth, cellHeight );
-function rotateCarousel() {
-    var angle = theta * selectedIndex * -1;
-    HS3DRotator.style.transform = "translateZ(" + -radius + "px) " + rotateFn + "(" + angle + "deg)";
-}
-var prevButton = document.querySelector('.hs-3d-rotator-prev');
-prevButton.addEventListener('click', function () {
-    selectedIndex--;
-    rotateCarousel();
-});
-var nextButton = document.querySelector('.hs-3d-rotator-next');
-nextButton.addEventListener('click', function () {
-    selectedIndex++;
-    rotateCarousel();
-});
-var cellsRange = document.querySelector('.hs-3d-rotator-cells-range');
-cellsRange.addEventListener('change', changeCarousel);
-cellsRange.addEventListener('input', changeCarousel);
-function changeCarousel() {
-    cellCount = cellsRange.value;
-    theta = 360 / cellCount;
-    var cellSize = isHorizontal ? cellWidth : cellHeight;
-    radius = Math.round((cellSize / 2) / Math.tan(Math.PI / cellCount));
-    for (var i = 0; i < cells.length; i++) {
-        var cell = cells[i];
-        if (i < cellCount) {
-            // visible cell
-            cell.style.opacity = 1;
-            var cellAngle = theta * i;
-            cell.style.transform = rotateFn + "(" + cellAngle + "deg) translateZ(" + radius + "px)";
+var rotatorContainer = document.querySelector('.hs-3drotate__container');
+            rotatorContainer.classList.add('hs-vanish');
+        var HSThreeDRotator=document.querySelector('.hs-3drotate');
+        
+        var cells=HSThreeDRotator.querySelectorAll('.hs-3drotate__cell');
+        var cellCount; // cellCount set from cells-range input value
+        var selectedIndex=0;
+        var cellWidth=HSThreeDRotator.offsetWidth;
+        var cellHeight=HSThreeDRotator.offsetHeight;
+        var isHorizontal=true;
+        var rotateFn=isHorizontal? 'rotateY':'rotateX';
+        var radius,theta;
+        // console.log( cellWidth, cellHeight );
+
+        function rotateCarousel() {
+          var angle=theta*selectedIndex*-1;
+          HSThreeDRotator.style.transform='translateZ('+-radius+'px) ' + rotateFn+'('+angle+'deg)';
         }
-        else {
-            // hidden cell
-            cell.style.opacity = 0;
-            cell.style.transform = 'none';
+
+        var prevButton=document.querySelector('.hs-3drotate__prev-button');
+        prevButton.addEventListener('click',function() {
+          selectedIndex--;
+          rotateCarousel();
+        });
+
+        var nextButton=document.querySelector('.hs-3drotate__next-button');
+        nextButton.addEventListener('click',function() {
+          selectedIndex++;
+          rotateCarousel();
+        });
+
+        var cellsRange=document.querySelector('.hs-3drotate__cells-range');
+        cellsRange.addEventListener('change',update3DRotator);
+        cellsRange.addEventListener('input',update3DRotator);
+
+
+
+        function update3DRotator() {
+          cellWidth=HSThreeDRotator.offsetWidth;
+          cellHeight=HSThreeDRotator.offsetHeight;
+          console.log(cellHeight);
+          cellCount=cellsRange.value;
+          theta=360/cellCount;
+          var cellSize=isHorizontal? cellWidth:cellHeight;
+          radius=Math.round((cellSize/2)/Math.tan(Math.PI/cellCount));
+          for(var i=0;i<cells.length;i++) {
+            var cell=cells[i];
+            if(i<cellCount) {
+              // visible cell
+              cell.style.opacity=1;
+              var cellAngle=theta*i;
+              cell.style.transform=rotateFn+'('+cellAngle+'deg) translateZ('+radius+'px)';
+            } else {
+              // hidden cell
+              cell.style.opacity=0;
+              cell.style.transform='none';
+            }
+          }
+
+          rotateCarousel();
+          
         }
-    }
-    rotateCarousel();
-}
-var orientationInputs = document.querySelectorAll('input[name="orientation"]');
-(function () {
-    for (var i = 0; i < orientationInputs.length; i++) {
-        var radio = orientationInputs[i];
-        radio.addEventListener('change', onOrientationChange);
-    }
-})();
-function onOrientationChange() {
-    var checkedRadioInput = document.querySelector('input[name="orientation"]:checked');
-    isHorizontal = checkedRadioInput.value == 'horizontal';
-    rotateFn = isHorizontal ? 'rotateY' : 'rotateX';
-    changeCarousel();
-}
-// set initials
-onOrientationChange();
+
+        var orientationRadios=document.querySelectorAll('input[name="orientation"]');
+        (function() {
+          for(var i=0;i<orientationRadios.length;i++) {
+            var radio=orientationRadios[i];
+            radio.addEventListener('change',onOrientationChange);
+          }
+        })();
+
+        function onOrientationChange() {
+          var checkedRadio=document.querySelector('input[name="orientation"]:checked');
+          isHorizontal=checkedRadio.value=='horizontal';
+          rotateFn=isHorizontal? 'rotateY':'rotateX';
+          update3DRotator();
+        }
+
+        // set initials 
+        onOrientationChange();
+        
+        setTimeout(() => {window.onresize = update3DRotator}, 500);
+        document.addEventListener('shown.bs.modal', () =>{
+          
+          rotatorContainer.classList.remove('hs-vanish');
+          update3DRotator();
+        });
--- src/js/modules/hs-3d-rotate/hs-3d-rotate.ts
+++ src/js/modules/hs-3d-rotate/hs-3d-rotate.ts
@@ -1,77 +1,89 @@
+var rotatorContainer = document.querySelector('.hs-3drotate__container');
+rotatorContainer.classList.add('hs-vanish');
+var HSThreeDRotator = document.querySelector('.hs-3drotate');
 
-        const HS3DRotator: HTMLDivElement = document.querySelector('.hs-3d-rotator');
-        const cells: NodeList = HS3DRotator.querySelectorAll('.hs-3d-rotator__cell');
-        let cellCount; // cellCount set from cells-range input value
-        let selectedIndex = 0;
-        const cellWidth = HS3DRotator.offsetWidth;
-        const cellHeight = HS3DRotator.offsetHeight;
-        let isHorizontal = true;
-        let rotateFn = isHorizontal ? 'rotateY':'rotateX';
-        let radius
-        let theta;
-        // console.log( cellWidth, cellHeight );
+var cells = HSThreeDRotator.querySelectorAll('.hs-3drotate__cell');
+var cellCount; // cellCount set from cells-range input value
+var selectedIndex = 0;
+var cellWidth = HSThreeDRotator.offsetWidth;
+var cellHeight = HSThreeDRotator.offsetHeight;
+var isHorizontal = true;
+var rotateFn = isHorizontal ? 'rotateY' : 'rotateX';
+var radius, theta;
+// console.log( cellWidth, cellHeight );
 
-        function rotateCarousel() {
-          const angle = theta * selectedIndex * -1;
-          HS3DRotator.style.transform=`translateZ(${-radius}px) ${rotateFn}(${angle}deg)`;
-        }
+function rotateCarousel() {
+  var angle = theta * selectedIndex * -1;
+  HSThreeDRotator.style.transform = 'translateZ(' + -radius + 'px) ' + rotateFn + '(' + angle + 'deg)';
+}
 
-        const prevButton = document.querySelector('.hs-3d-rotator-prev');
-        prevButton.addEventListener('click',function() {
-          selectedIndex--;
-          rotateCarousel();
-        });
+var prevButton = document.querySelector('.hs-3drotate__prev-button');
+prevButton.addEventListener('click', function() {
+  selectedIndex--;
+  rotateCarousel();
+});
 
-        const nextButton=document.querySelector('.hs-3d-rotator-next');
-        nextButton.addEventListener('click',function() {
-          selectedIndex++;
-          rotateCarousel();
-        });
+var nextButton = document.querySelector('.hs-3drotate__next-button');
+nextButton.addEventListener('click', function() {
+  selectedIndex++;
+  rotateCarousel();
+});
 
-        const cellsRange: HTMLInputElement = document.querySelector('.hs-3d-rotator-cells-range');
-        cellsRange.addEventListener('change',changeCarousel);
-        cellsRange.addEventListener('input',changeCarousel);
+var cellsRange = document.querySelector('.hs-3drotate__cells-range');
+cellsRange.addEventListener('change', update3DRotator);
+cellsRange.addEventListener('input', update3DRotator);
 
 
 
-        function changeCarousel() {
-          cellCount = cellsRange.value;
-          theta = 360/cellCount;
-          const cellSize = isHorizontal ? cellWidth: cellHeight;
-          radius = Math.round((cellSize / 2) / Math.tan(Math.PI / cellCount));
-          for(let i = 0; i < cells.length; i++) {
-            const cell: any = cells[i];
-            if(i < cellCount) {
-              // visible cell
-              cell.style.opacity = 1;
-              const cellAngle = theta * i;
-              cell.style.transform = `${rotateFn}(${cellAngle}deg) translateZ(${radius}px)`;
-            } else {
-              // hidden cell
-              cell.style.opacity = 0;
-              cell.style.transform = 'none';
-            }
-          }
+function update3DRotator() {
+  cellWidth = HSThreeDRotator.offsetWidth;
+  cellHeight = HSThreeDRotator.offsetHeight;
+  console.log(cellHeight)
+  cellCount = cellsRange.value;
+  theta = 360 / cellCount;
+  var cellSize = isHorizontal ? cellWidth : cellHeight;
+  radius = Math.round((cellSize / 2) / Math.tan(Math.PI / cellCount));
+  for (var i = 0; i < cells.length; i++) {
+    var cell = cells[i];
+    if (i < cellCount) {
+      // visible cell
+      cell.style.opacity = 1;
+      var cellAngle = theta * i;
+      cell.style.transform = rotateFn + '(' + cellAngle + 'deg) translateZ(' + radius + 'px)';
+    } else {
+      // hidden cell
+      cell.style.opacity = 0;
+      cell.style.transform = 'none';
+    }
+  }
 
-          rotateCarousel();
-        }
+  rotateCarousel();
 
-        const orientationInputs=document.querySelectorAll('input[name="orientation"]');
-        (function() {
-          for(let i = 0; i < orientationInputs.length; i++) {
-            const radio=orientationInputs[i];
-            radio.addEventListener('change',onOrientationChange);
-          }
-        })();
+}
 
-        function onOrientationChange() {
-          const checkedRadioInput: HTMLInputElement = document.querySelector('input[name="orientation"]:checked');
-          isHorizontal = checkedRadioInput.value == 'horizontal';
-          rotateFn = isHorizontal ? 'rotateY':'rotateX';
-          changeCarousel();
-        }
+var orientationRadios = document.querySelectorAll('input[name="orientation"]');
+(function() {
+  for (var i = 0; i < orientationRadios.length; i++) {
+    var radio = orientationRadios[i];
+    radio.addEventListener('change', onOrientationChange);
+  }
+})();
 
-        // set initials
-        onOrientationChange();
+function onOrientationChange() {
+  var checkedRadio = document.querySelector('input[name="orientation"]:checked');
+  isHorizontal = checkedRadio.value == 'horizontal';
+  rotateFn = isHorizontal ? 'rotateY' : 'rotateX';
+  update3DRotator();
+}
 
-      
+// set initials 
+onOrientationChange();
+
+setTimeout(() => {
+  window.onresize = update3DRotator
+}, 500);
+document.addEventListener('shown.bs.modal', () => {
+
+  rotatorContainer.classList.remove('hs-vanish');
+  update3DRotator();
+});
