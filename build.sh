30ac8ae82500d7fcfc69737458f039dad4129b3f
heathshults <heatheshults@gmail.com>
2021-02-24 18:52:09 -0600

    build system

--- build.sh
+++ build.sh
@@ -12,8 +12,8 @@ echo "Build initializing..."
 # echo "Typechecking [.js, .jsx, .ts, .tsx] files..." 
 tsc --project ./tsconfig.typecheck.json 
 
-# echo "Build Stencil Components..." &&
-npx stencil build --dev --docs-readme --debug --watch &
+# echo "Build Stencil Components..."
+npx stencil build --dev --docs-readme --debug &
 # STENCILPID=$! # get background process id of stencil
 
 # wait $STENCILPID
@@ -21,6 +21,7 @@ npx stencil build --dev --docs-readme --debug --watch &
 # npx babel system-modules/scss2cssVars/scss2cssVars.ts --out-file system-modules/scss2cssVars/scss2cssVars.js
 npx babel src/js/modules --out-dir src/js/temp/ 
 npx babel src/js/modules --out-file www-app/assets/js/HeathShults.built.js 
+npx browserify src/js/temp/HeathShults.built.js -o www-app/assets/js/HeathShults.built.js -t [ babelify --presets [ @babel/preset-env @babel/preset-react @babel/preset-typescript ] ]
 
 # echo "Browserify Javascript..." 
 # npx browserify src/index.js > www-app/js 
@@ -38,20 +39,20 @@ node build-scripts/build-scss.js
 # npx postcss www-app/assets/css/*.css --use autoprefixer -d www-app/assets/css &&
 
 echo "Copying supporting files..." 
-npx copy www/components/**/* www-app/components &
-npx copy src/assets/content/**/* www-app/assets/content &
-npx copy src/assets/css/**/* www-app/assets/css &
-# npx copy src/assets/fa-5140/**/* www-app/assets/fa-5140
-npx copy src/assets/img/**/* www-app/assets/img &
-npx copy src/assets/webfonts/**/* www-app/assets/webfonts &
-npx copy src/js/temp/**/* www-app/assets/js/vendor &
-npx copy src/js/vendor/**/* www-app/assets/js/vendor &
-npx copy www/sw.js www-app &
-npx copy src/global/**/* www-app/global &
-npx copy src/php/**/* www-app/php &
-npx copy src/mail/**/* www-app/mail &
-npx copy src/favicon.ico www-app &
 
+npx copy src/assets/content/**/* www-app/assets/content &&
+npx copy src/assets/css/**/* www-app/assets/css &&
+# npx copy src/assets/fa-5140/**/* www-app/assets/fa-5140
+npx copy src/assets/img/**/* www-app/assets/img &&
+npx copy src/assets/webfonts/**/* www-app/assets/webfonts &&
+npx copy src/js/temp/**/* www-app/assets/js/vendor &&
+npx copy src/js/vendor/**/* www-app/assets/js/vendor &&
+npx copy www/sw.js www-app &&
+npx copy src/global/**/* www-app/global &&
+npx copy src/php/**/* www-app/php &&
+npx copy src/mail/**/* www-app/mail &&
+npx copy src/favicon.ico www-app &&
+npx copy www/components/**/* www-app/components &
 
 echo "Starting development server..."
 npx gulp connect_sync &
