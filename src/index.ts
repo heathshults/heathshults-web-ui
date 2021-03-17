99f7aa948d3012daa8942fcef815d7396c4b7c11
heathshults <heatheshults@gmail.com>
2021-03-17 00:14:27 -0500

    flipcode

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,20 @@ import './js/modules/validate-url/validate-url.ts';
 
 import './js/modules/HeathScript.js';
 
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+// test-component is the name of our made up Web Component that we have
+// published to npm:
+import { applyPolyfills, defineCustomElements } from 'test-components/loader';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
+
+applyPolyfills().then(() => {
+  defineCustomElements();
+});
