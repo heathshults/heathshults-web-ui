8d132a5160f2c554700b298e0c24462b6803d1bc
heathshults <heatheshults@gmail.com>
2021-03-24 23:54:58 -0500

    slide button

--- src/js/modules/HeathScript.js
+++ src/js/modules/HeathScript.js
@@ -45,52 +45,52 @@ window.onscroll = function scrollFunction() {
 };
 
 
- // *====== DRAG and Drop Submit *======//
+//  // *====== DRAG and Drop Submit *======//
 
 
-  window.onload = function anibutton() {
-  //animated checkbox
-    let animatedCheckMark = 
-      `<svg class="svg-checkmark" x="0px" y="0px" viewBox="0 0 135 110" width="40px" height="50px">
-          <path class="check" d="M126.8,14L55.7,85.1L29.2,63.4"/>
-        </svg>`;
+//   window.onload = function anibutton() {
+//   //animated checkbox
+//     let animatedCheckMark = 
+//       `<svg class="svg-checkmark" x="0px" y="0px" viewBox="0 0 135 110" width="40px" height="50px">
+//           <path class="check" d="M126.8,14L55.7,85.1L29.2,63.4"/>
+//         </svg>`;
       
     
-    let dz = document.querySelector('.hs-dropzone');
-    let arrows = document.querySelector('#arrows');
-    function onAllowDrop(ev) {
-      ev.preventDefault();
-      // dz.style.cursor = 'move';
-      if (!dz.classList.contains('hs-dropzone-hover')) {
-        dz.classList.toggle('hs-dropzone-hover');
-      }
-      if (!arrows.classList.contains('hs-fadeout')){
-        arrows.classList.add('hs-fadeout');
-      }
-    }
-    function onDragLeave(ev) {
-      if (arrows.classList.contains('hs-fadeout')){
-        arrows.classList.remove('hs-fadeout');
-      }
-    }
+//     let dz = document.querySelector('.hs-dropzone');
+//     let arrows = document.querySelector('#arrows');
+//     function onAllowDrop(ev) {
+//       ev.preventDefault();
+//       // dz.style.cursor = 'move';
+//       if (!dz.classList.contains('hs-dropzone-hover')) {
+//         dz.classList.toggle('hs-dropzone-hover');
+//       }
+//       if (!arrows.classList.contains('hs-fadeout')){
+//         arrows.classList.add('hs-fadeout');
+//       }
+//     }
+//     function onDragLeave(ev) {
+//       if (arrows.classList.contains('hs-fadeout')){
+//         arrows.classList.remove('hs-fadeout');
+//       }
+//     }
 
    
-    function onDrag(ev) {
-      ev.dataTransfer.setData("text", ev.target.id);
-      ev.currentTarget.style.cursor = 'move';
-      //  ev.currentTarget.style.background = 'yellow';
-    }
+//     function onDrag(ev) {
+//       ev.dataTransfer.setData("text", ev.target.id);
+//       ev.currentTarget.style.cursor = 'move';
+//       //  ev.currentTarget.style.background = 'yellow';
+//     }
    
-    function onDrop(ev) {
-      ev.preventDefault();
-      var data = ev.dataTransfer.getData("text");
-      ev.target.appendChild(document.getElementById(data));
-      document.getElementById(data).classList.add('hs-fadeout');
-      dz.classList.add('hs-dropzone-has-file');
-    //  dz.innerHTML = animatedCheckMark;
-    let checky = document.createElement('span');
-    checky.classList.add('hs-animated-checkbox-container');
-    checky.innerHTML = animatedCheckMark;
-    dz.appendChild(checky);
-    }
-  };
+//     function onDrop(ev) {
+//       ev.preventDefault();
+//       var data = ev.dataTransfer.getData("text");
+//       ev.target.appendChild(document.getElementById(data));
+//       document.getElementById(data).classList.add('hs-fadeout');
+//       dz.classList.add('hs-dropzone-has-file');
+//     //  dz.innerHTML = animatedCheckMark;
+//     let checky = document.createElement('span');
+//     checky.classList.add('hs-animated-checkbox-container');
+//     checky.innerHTML = animatedCheckMark;
+//     dz.appendChild(checky);
+//     }
+//   };
